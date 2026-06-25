5078df5ba29a4ae70db573d41a9a3d9754b53326
Dhandapani <104203898+daniuae@users.noreply.github.com>
2026-06-25 06:29:53 +0530

    Create DataTypeExample.java

---
+++ Root/Java/Programs/DataTypeExample.java
@@ -0,0 +1,12 @@
+public class PrimitiveExample {
+ public static void main(String[] args) {
+ byte age = 30;
+ int population = 1420000000;
+ long distanceToStar = 9460730472580800L;
+
+ float rate = 4.5F;
+ double temperature = 36.625;
+ char grade = 'A';
+ boolean isActive = true;
+ }
+}
